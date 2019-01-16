f13f919493bf4f9d674f21f3d3ddf91114e01807
Mukesh Dangi <mukeshkdangi@users.noreply.github.com>
2019-01-16 13:52:38 -0800

    Add files via upload

--- hw1_knn.py
+++ hw1_knn.py
@@ -19,33 +19,77 @@ class KNN:
 
     #TODO: Complete the training function
     def train(self, features: List[List[float]], labels: List[int]):
-        print()
-        #raise NotImplementedError
+        self.train_features = features
+        self.train_labels = labels
         
     
     #TODO: Complete the prediction function
     def predict(self, features: List[List[float]]) -> List[int]:
-        print()
-        #raise NotImplementedError
+        prediction_labels = []
+        for i in range(len(features)):
+            k_neighbore_lables = self.get_k_neighbors(features[i])
+            prediction_labels.append(self.get_near_by_majority_label(k_neighbore_lables))
+        return prediction_labels
         
     #TODO: Complete the get k nearest neighbor function
     def get_k_neighbors(self, point):
-        print()
-        #raise NotImplementedError
+        nearestNeighbors = []
+        kNeighborsLabels = []
+        for idx in range(len(self.train_features)):
+            dist_value = self.distance_function(point, self.train_features[idx])
+            nearestNeighbors.append([dist_value, self.train_labels[idx]])
+
+        nearestNeighbors.sort(key=lambda idx: idx[0])
+        count=0
+        while(count<self.k):
+            kNeighborsLabels.append(nearestNeighbors[count][1])
+            count = count+1   
+        return kNeighborsLabels
+    
+    def get_near_by_majority_label(self, k_neighbore_lables) -> int:
+        negative_label = 0
+        positive_label = 0
+        for x in range(len(k_neighbore_lables)):
+            if(k_neighbore_lables[x] == 0):
+                negative_label = negative_label+1
+            else:
+                positive_label = positive_label+1
+
+        if(negative_label>positive_label):
+            return 0
+        else:
+            return 1
+        
         
     #TODO: Complete the model selection function where you need to find the best k     
-    def model_selection_without_normalization(distance_funcs, Xtrain, ytrain, f1_score, Xval, yval, Xtest, ytest):
+    def model_selection_without_normalization(distance_funcs, train_features, train_labels, f1_score, valid_features, valid_labels, test_features, test_labels):
         
             
             #Dont change any print statement
-            print('[part 1.1] {name}\tk: {k:d}\t'.format(name=name, k=k) + 
-                      'train: {train_f1_score:.5f}\t'.format(train_f1_score=train_f1_score) +
-                      'valid: {valid_f1_score:.5f}'.format(valid_f1_score=valid_f1_score))
-    
-            print()
-            print('[part 1.1] {name}\tbest_k: {best_k:d}\t'.format(name=name, best_k=best_k) +
-                  'test f1 score: {test_f1_score:.5f}'.format(test_f1_score=test_f1_score))
-            print()
+            for name, func in distance_funcs.items():
+                best_f1_score, best_k = -1, 0
+                for k in [1, 3, 10, 20, 50]:
+                    model = KNN(k=k, distance_function=func)
+                    model.train(train_features, train_labels)
+                    train_f1_score = f1_score(train_labels,  model.predict(train_features))
+                    valid_f1_score = f1_score(valid_labels, model.predict(valid_features))
+                    
+                    
+                    print('[part 1.1] {name}\tk: {k:d}\t'.format(name=name, k=k) + 
+                  'train: {train_f1_score:.5f}\t'.format(train_f1_score=train_f1_score) +
+                  'valid: {valid_f1_score:.5f}'.format(valid_f1_score=valid_f1_score))
+            
+                    if valid_f1_score > best_f1_score:
+                        best_f1_score, best_k,model = valid_f1_score, k,model
+
+                model = KNN(k=best_k, distance_function=func)
+                model.train(train_features,train_labels)
+                test_f1_score = f1_score(test_labels, model.predict(test_features))
+                print()
+
+                print('[part 1.1] {name}\tbest_k: {best_k:d}\t'.format(name=name, best_k=best_k) +
+                          'test f1 score: {test_f1_score:.5f}'.format(test_f1_score=test_f1_score))
+                print()
             return best_k, model
     
     #TODO: Complete the model selection function where you need to find the best k with transformation
